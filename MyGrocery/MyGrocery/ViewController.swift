61081ab4c5047941301753aa1dd6a1ef6fa28435
Martin Ivanov <martin.ivanov@mentormate.com>
2024-07-11 17:51:58 +0300

    Inital Commit

---
+++ MyGrocery/MyGrocery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGrocery
+//
+//  Created by Martin Ivanov on 7/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
